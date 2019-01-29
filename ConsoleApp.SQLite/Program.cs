c3fda3513f42663ca734abc8b8b8cfe3684aa106
papitomasterdeveloper <papitomasterdeveloper@gmail.com>
2019-01-29 17:41:16 -0600

    Added a model.cs, created the migrations and tables

---
+++ ConsoleApp.SQLite/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace ConsoleApp.SQLite
+{
+    public class Program
+    {
+        public static void Main()
+        {
+            // New instance of BloggingContext from DbContext
+            using (var db = new BloggingContext())
+            {
+                // From Blogs table, add a new Blog instance Collection
+                db.Blogs.Add(new Blog { Url = "http://blogs.msdn.com/adonet" });
+                var count = db.SaveChanges();
+                Console.WriteLine("{0} records saved to database", count);
+
+                Console.WriteLine();
+                Console.WriteLine("All blogs in database:");
+                foreach (var blog in db.Blogs)
+                {
+                    Console.WriteLine(" - {0}", blog.Url);
+                }
+            }
+        }
+    }
+}
